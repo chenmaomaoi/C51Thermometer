d1f176c60c2651e126755d9b7a977b5f8762b320
橘子皮 <1325242398@qq.com>
2022-02-23 09:57:00 +0800

    Update main.c

--- main.c
+++ main.c
@@ -7,21 +7,12 @@
 #include "stdio.h"
 #include "ASCII.h"
 
-void EventHandler_UART_RecvByte(const unsigned char dat)
-{
-	static unsigned char buffer[64];
-	static char index = 0;
+const char* CONNECT_SERVER_STR = "AT+CIPSTART=\"TCP\",\"cn-zz-bgp-4.natfrp.cloud\",16967\r\n";
 
-	if (dat == '\r' || index > sizeof(buffer) - 1)
-	{
-		buffer[index] = NUL;
-		LCD_1602_Clear();
-		LCD_1602_ShowString(0, 0, buffer);
-		index = 0;
-		return;
-	}
-	buffer[index] = dat;
-	index++;
+void EventHandler_UART_RecvdStr(const unsigned char* str)
+{
+	LCD_1602_Clear();
+	LCD_1602_ShowString(0, 0, str);
 }
 
 void delay1s(void)   //Îó²î -0.000000001137us
@@ -36,27 +27,28 @@ void delay1s(void)   //Îó²î -0.000000001137us
 void main()
 {
 	char ch[10];
-	Event_UART_RecvdByte = &EventHandler_UART_RecvByte;
+	Event_UART_RecvdStr = &EventHandler_UART_RecvdStr;
+
+	IIC_Init();
+	LCD_1602_Init();
 
 	UART_Init();
 	EA = 1;
 
-	IIC_Init();
-	LCD_1602_Init();
 	SHT_30_Init();
 
 	//UART_SendString("AT\r\n" + EOT);
 
 	while (1)
 	{
-		if (SHT_30_DataProcess())
-		{
-			sprintf(ch, "T:%.2f ", SHT_30_T);
-			LCD_1602_ShowString(1, 0, ch);
+		//if (SHT_30_DataProcess())
+		//{
+		//	sprintf(ch, "T:%.2f ", SHT_30_T);
+		//	LCD_1602_ShowString(1, 0, ch);
 
-			sprintf(ch, "RH:%.2f", SHT_30_RH);
-			LCD_1602_ShowString(1, 8, ch);
-		}
+		//	sprintf(ch, "RH:%.2f", SHT_30_RH);
+		//	LCD_1602_ShowString(1, 8, ch);
+		//}
 
 		delay1s();
 	}

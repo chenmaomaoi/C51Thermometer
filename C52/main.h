1f7f9b68088f66fc03058385eafb72a0420498ba
橘子皮 <1325242398@qq.com>
2022-04-13 17:21:03 +0800

    Add string.h

--- C52/main.h
+++ C52/main.h
@@ -5,10 +5,6 @@
 #include "reg52.h"
 #endif // !_REG52_H
 
-#ifndef _INTRINS_H
-#include "intrins.h"
-#endif // !_INTRINS_H
-
 #ifndef _UART_H
 #include "UART.h"
 #endif // !_UART_H
@@ -16,10 +12,6 @@
 #ifndef _IIC_H
 #include "IIC.h"
 #endif // !_IIC_H
-
-//#ifndef _LCD_1602_H
-//#include "LCD_1602.h"
-//#endif // !_LCD_1602_H
 
 #ifndef _SHT_30_H
 #include "SHT_30.h"

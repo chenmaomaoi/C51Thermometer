c3cbf8a6c68f998b58d259e7840919701c26a6d9
橘子皮 <1325242398@qq.com>
2022-05-03 20:43:10 +0800

    9600双倍速

--- C52/UART.c
+++ C52/UART.c
@@ -1,17 +1,5 @@
 #include "UART.h"
 
-/*Define UART parity mode*/
-#define NONE_PARITY     0   //None parity
-#define ODD_PARITY      1   //Odd parity,奇校验
-#define EVEN_PARITY     2   //Even parity,偶校验
-#define MARK_PARITY     3   //Mark parity,1校验
-#define SPACE_PARITY    4   //Space parity,0校验
-
-#define FOSC 11059200L      //System frequency
-#define BAUD 9600           //UART baudrate
-
-#define PARITYBIT NONE_PARITY   //Testing even parity
-
 void (*UART_Event_RecvdStr)(const unsigned char* str);
 
 /// <summary>
@@ -20,18 +8,14 @@ void (*UART_Event_RecvdStr)(const unsigned char* str);
 /// </summary>
 void UART_Init()
 {
-#if (PARITYBIT == NONE_PARITY)
-	SCON = 0x50;            //8-bit variable UART
-#elif (PARITYBIT == ODD_PARITY) || (PARITYBIT == EVEN_PARITY) || (PARITYBIT == MARK_PARITY)
-	SCON = 0xda;            //9-bit variable UART, parity bit initial to 1
-#elif (PARITYBIT == SPACE_PARITY)
-	SCON = 0xd2;            //9-bit variable UART, parity bit initial to 0
-#endif
-
-	TMOD = 0x20;            //Set Timer1 as 8-bit auto reload mode
-	TH1 = TL1 = -(FOSC / 12 / 32 / BAUD); //Set auto-reload vaule
-	TR1 = 1;                //Timer1 start run
-	ES = 1;                 //Enable UART interrupt
+	TMOD = 0x20;
+	SCON = 0x50;
+	TH1 = 0xF4;
+	TL1 = TH1;
+	PCON = 0x80;
+	//EA = 1;
+	ES = 1;
+	TR1 = 1;
 }
 
 /// <summary>
